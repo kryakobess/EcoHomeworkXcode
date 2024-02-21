4dad80123553ce2c9f765cc2a888b3bbfc7655cd
Nikita Kuritsyn <martyz89@yandex.ru>
2024-02-21 19:29:52 +0300

    implement gnome_sort

---
+++ AssemblyFiles/Mac/Xcode_12_3/Build/Intermediates.noindex/Eco.Lab1.build/Debug/Static.build/Objects-normal/arm64/CEcoLab1.d
@@ -0,0 +1,14 @@
+dependencies: \
+  /Users/nikitakuritsyn/Documents/Eco.Lab1/SourceFiles/CEcoLab1.c \
+  /Users/nikitakuritsyn/Documents/EcoHW/Eco.Framework/Eco.Core1/SharedFiles/IEcoSystem1.h \
+  /Users/nikitakuritsyn/Documents/EcoHW/Eco.Framework/Eco.Core1/SharedFiles/IEcoBase1.h \
+  /Users/nikitakuritsyn/Documents/EcoHW/Eco.Framework/Eco.Core1/SharedFiles/depend.h \
+  /Users/nikitakuritsyn/Documents/EcoHW/Eco.Framework/Eco.Core1/SharedFiles/byteswap.h \
+  /Users/nikitakuritsyn/Documents/EcoHW/Eco.Framework/Eco.Core1/SharedFiles/atomic.h \
+  /Users/nikitakuritsyn/Documents/EcoHW/Eco.Framework/Eco.InterfaceBus1/SharedFiles/IEcoInterfaceBus1.h \
+  /Users/nikitakuritsyn/Documents/EcoHW/Eco.Framework/Eco.InterfaceBus1/SharedFiles/IEcoInterfaceBus1MemExt.h \
+  /Users/nikitakuritsyn/Documents/Eco.Lab1/HeaderFiles/CEcoLab1.h \
+  /Users/nikitakuritsyn/Documents/Eco.Lab1/SharedFiles/IEcoLab1.h \
+  /Users/nikitakuritsyn/Documents/EcoHW/Eco.Framework/Eco.MemoryManager1/SharedFiles/IdEcoMemoryManager1.h \
+  /Users/nikitakuritsyn/Documents/EcoHW/Eco.Framework/Eco.MemoryManager1/SharedFiles/IEcoMemoryManager1.h \
+  /Users/nikitakuritsyn/Documents/EcoHW/Eco.Framework/Eco.MemoryManager1/SharedFiles/IEcoMemoryAllocator1.h
